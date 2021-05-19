2c042034c26c9c17efdfea7b1f993d8aa73d44d7
Heena Jabeen (Trianz) <heena.jabeen@trianz.com>
2021-05-19 19:51:36 +0530

    Added Expenses.css and updated Expenses.js

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,30 +1,19 @@
 import React from "react";
 import ExpenseItem from "./ExpenseItem";
-//import "../css/Expenses.css";
+import "../css/Expenses.css";
 
 const Expenses = (props) => {
   return (
-    <div>
-      <ExpenseItem
-        title={props.expenses[0].title}
-        date={props.expenses[0].date}
-        amount={props.expenses[0].amount}
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        date={props.expenses[1].date}
-        amount={props.expenses[1].amount}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        date={props.expenses[2].date}
-        amount={props.expenses[2].amount}
-      />
-      <ExpenseItem
-        title={props.expenses[3].title}
-        date={props.expenses[3].date}
-        amount={props.expenses[3].amount}
-      />
+    <div className="expenses">
+      {props.expenses.map((item) => {
+        return (
+          <ExpenseItem
+            title={item.title}
+            date={item.date}
+            amount={item.amount}
+          />
+        );
+      })}
     </div>
   );
 };
